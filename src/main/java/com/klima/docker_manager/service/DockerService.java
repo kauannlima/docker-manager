fad8e567bbbb88492de947b4b0963ba6a8bdd80a
Kauan Lima <kauanalmeidalima1405@gmail.com>
2024-10-01 10:58:51 -0300

    Adicionando o 'DockerService'

--- src/main/java/com/klima/docker_manager/service/DockerService.java
+++ src/main/java/com/klima/docker_manager/service/DockerService.java
@@ -1,8 +1,11 @@
 package com.klima.docker_manager.service;
 
 import com.github.dockerjava.api.DockerClient;
-import lombok.AllArgsConstructor;
+import com.github.dockerjava.api.model.Container;
+import com.github.dockerjava.api.model.Image;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DockerService {
@@ -12,6 +15,27 @@ public class DockerService {
         this.dockerClient = client;
     }
 
-    public 
+    public List<Container> listContainers(boolean all){
+        return dockerClient.listContainersCmd().withShowAll(all).exec();
+    }
 
+    public List<Image> listImages(){
+        return dockerClient.listImagesCmd().exec();
+    }
+
+    public void startContainer(String containerId){
+        dockerClient.startContainerCmd(containerId).exec();
+    }
+
+    public void stopContainer(String containerId){
+        dockerClient.stopContainerCmd(containerId).exec();
+    }
+
+    public void deleteContainer(String containerId){
+        dockerClient.removeContainerCmd(containerId).exec();
+    }
+
+    public void createContainer(String imageName){
+        dockerClient.createContainerCmd(imageName).exec();
+    }
 }
